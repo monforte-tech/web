83934c39128157b1fd18b907f28c7af15ffb397a
Jorge Monforte González <yo@llou.net>
2025-07-21 20:23:51 +0200

    add sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: passthroughImageService(),
-  },
   site: "https://monforte-tech.github.io/web/",
 });
