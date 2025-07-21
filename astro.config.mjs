b90ea4cb5f1d24658059a65949adfd94448f47e8
Jorge Monforte González <yo@llou.net>
2025-07-21 19:47:15 +0200

    Disabel sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
+  site: "https://montech.github.io",
+});
