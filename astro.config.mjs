1733a25b6498b428921046fcecca327e318e77e4
Jorge Monforte González <yo@llou.net>
2025-07-21 20:26:30 +0200

    Move page to root

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://monforte-tech.github.io/web/",
+  site: "https://monforte-tech.github.io/",
 });
