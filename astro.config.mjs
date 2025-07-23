487bf47a35fecd06e066ba661af0549a283ab225
Jorge Monforte González <yo@llou.net>
2025-07-23 05:38:01 +0200

    move from /web to /

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/web",
+  base: "/",
   site: "https://monforte-tech.github.io/",
 });
