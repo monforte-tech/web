16c6625b5d2bfb6efc4cd99d72999aa5d0986b42
Jorge Monforte González <yo@llou.net>
2025-07-24 19:28:34 +0200

    floating logo on the works

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/web",
+  base: "/web/",
   site: "https://monforte-tech.github.io/",
 });
